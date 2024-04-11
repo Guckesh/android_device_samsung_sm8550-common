281512b95d402dfbed22e8d0764b306063438866
Mezaque <mezaquegit@gmail.com>
2024-04-10 21:06:45 -0300

    sm8550-common: Drop android.hardware.power-service-qti HAL It doesn't work
    * E QTI PowerHAL: Failed to perform launch boost
    * E QTI PowerHAL: Failed to acquire lock.

--- common.mk
+++ common.mk
@@ -324,7 +324,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.3.vendor
 
