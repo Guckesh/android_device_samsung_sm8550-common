48e5deead736365fe7514197ef5763f1b45f144c
Luka Panio <lukapanio@gmail.com>
2024-04-13 21:17:45 -0300

    sm8550-common: Copy init.recovery.qcom.rc to recovery root As it is imported from root
    Change-Id: I26c89841ca0012f5694896810816f8e1f43ad180

--- common.mk
+++ common.mk
@@ -349,6 +349,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/init.recovery.qcom.rc:root/init.recovery.qcom.rc
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2.vendor \
