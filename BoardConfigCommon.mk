4d75d19d1b34ed84d2c593a1b98188e3bb7ecdd8
Guckesh <mezaquegit@gmail.com>
2024-04-15 18:10:11 -0300

    sm8550-common: Switch to soong_config_set android_hardware_audio run_64bit

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,6 +26,8 @@ TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
 
 # Audio
+$(call soong_config_set, android_hardware_audio, run_64bit, true)
+
 AUDIO_FEATURE_ENABLED_DLKM := true
 AUDIO_FEATURE_ENABLED_GEF_SUPPORT := true
 AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
