4a3a29b0b8d6082819301de3ee8b5ce7f061b831
Guckesh <mezaquegit@gmail.com>
2024-06-27 19:57:16 -0300

    sm8550-common: Drop duplicate PACKAGE_OVERLAYS

--- common.mk
+++ common.mk
@@ -311,10 +311,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
-# Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 DEVICE_PACKAGE_OVERLAYS += \
